f55e4c49828c7048e4a8fd87b519bb5df4a19d1b
Peter Chapman <pychapster@gmail.com>
2017-10-09 17:52:47 -0700

    Live reload working
    Added config.rb file, changes to gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.initConfig({
     uglify: {
       my_target: {
@@ -9,12 +10,23 @@ module.exports = function(grunt) {
         } // files
       }  //my_target
     },  //uglify
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
+        } // options
+      } // dev
+    },  // compass
     watch: {
       options: {livereload: true},
       scripts: {
             files: ['_/components/js/*.js'],
             tasks: ['uglify']
           }, // script
+          sass: {
+            files: ['_/components/sass/*.scss'],
+            tasks: ['compass:dev']
+          },  // sass
           html: {
             files: ['*.html']
           }
