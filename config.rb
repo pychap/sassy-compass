7d0c8a7c48c8cd77d24bf2f6020e0deaf9b547b4
pychap <pychapster@gmail.com>
2017-10-22 17:24:51 -0700

    Final steps

--- config.rb
+++ config.rb
@@ -2,5 +2,6 @@ require "susy"
 css_dir = '_/css'
 sass_dir = '_/components/sass'
 javascripts_dir = '_/js'
-output_style = :compressed
+output_style = :expanded
+# do output_style = :compressed for production
 relative_assets = true;
